73cce2bb94a590614c9080312d90a7acaf459818
AlkBur <alkboor@mail.ru>
2016-05-08 01:15:57 +0300

    Added changes

--- main.py
+++ main.py
@@ -1,32 +1,36 @@
 import asyncore
 import socket
 
+# Server - асинхронный echo сервер
 class Server(asyncore.dispatcher):
     def __init__(self, host, port):
         asyncore.dispatcher.__init__(self)
         self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.set_reuse_addr()
         self.bind((host, port))
         self.listen(10)
 
     def handle_accept(self):
         # when we get a client connection start a dispatcher for that
         # client
-        socket, address = self.accept()
-        print 'Connection by', address
-        EchoHandler(socket)
+        pair = self.accept()
+        if pair is not None:
+            socket, address = pair
+            print 'Connection by', address
+            EchoHandler(socket)
 
+# EchoHandler - Обработка полученных данных
 class EchoHandler(asyncore.dispatcher_with_send):
     # dispatcher_with_send extends the basic dispatcher to have an output
     # buffer that it writes whenever there's content
     def handle_read(self):
-        self.out_buffer = self.recv(1024)
-        print 'received', self.out_buffer
-        if not self.out_buffer:
-            self.close()
-        if self.out_buffer == "Close" or self.out_buffer == "close":
-            self.close()
-        else:
-            self.send(self.out_buffer)
+        data = self.recv(1024)
+        print 'received = ', data
+        if data:
+            if data == "Close" or data == "close":
+                self.close()
+            else:
+                self.send(data)
 
 s = Server('0.0.0.0', 2222)
 asyncore.loop()
